83746dde353f9f2082e1fae5a84d7764b9bc0040
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-05-13 12:38:42 +0200

    Modernise project integration with setuptools using `setup.cfg` file

--- setup.py
+++ setup.py
@@ -1,33 +1,48 @@
-from setuptools import setup
-from codecs import open
-from os import path
-from Cython.Build import cythonize
+import configparser
+import sys
+
+import setuptools
 import numpy
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the relevant file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+from Cython.Build import cythonize
+from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools.command.sdist import sdist as _sdist
 
 
-setup(
-    name='pyhcrf',
-    version='0.0.1',
-    packages=['pyhcrf'],
-    install_requires=['numpy>=1.9'],
-    ext_modules=cythonize('pyhcrf/algorithms.pyx'),
-    include_dirs=[numpy.get_include()],
-    url='https://github.com/dirko/pyhcrf',
-    license='GPL',
-    author='Dirko Coetsee',
-    author_email='dpcoetsee@gmail.com',
-    description='Hidden conditional random field, a sequence classifier',
-    long_description=long_description,
-    classifiers=[
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GPL License',
-        'Programming Language :: Python',
-        'Topic :: Scientific/Engineering',
-        ],
-    )
+class sdist(_sdist):
+    """A `sdist` that generates a `pyproject.toml` on the fly.
+    """
+
+    def run(self):
+        # build `pyproject.toml` from `setup.cfg`
+        c = configparser.ConfigParser()
+        c.add_section("build-system")
+        c.set("build-system", "requires", str(self.distribution.setup_requires))
+        c.set("build-system", 'build-backend', '"setuptools.build_meta"')
+        with open("pyproject.toml", "w") as pyproject:
+            c.write(pyproject)
+        # run the rest of the packaging
+        _sdist.run(self)
+
+
+class build_ext(_build_ext):
+    """A `build_ext` that disables optimizations if compiled in debug mode.
+    """
+
+    def build_extension(self, ext):
+        if self.debug:
+            ext.extra_compile_args.append("-O0")
+            if sys.implementation.name == "cpython":
+                ext.define_macros.append(("CYTHON_TRACE_NOGIL", 1))
+        _build_ext.build_extension(self, ext)
+
+
+setuptools.setup(
+    ext_modules=cythonize([
+        setuptools.Extension(
+            "pyhcrf._algorithms",
+            ["pyhcrf/_algorithms.pyx"],
+            include_dirs=[numpy.get_include()],
+            extra_compile_args=["-Wall"],
+        ),
+    ]),
+)
