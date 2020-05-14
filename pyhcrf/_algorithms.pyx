093a973048467bc2f12d163a3f0b64cbaeb8ae23
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-05-14 02:23:59 +0200

    Reformat `_algorithms.pyx` using platform independent types and nogil where applicable

--- pyhcrf/_algorithms.pyx
+++ pyhcrf/_algorithms.pyx
@@ -1,11 +1,12 @@
 # coding: utf-8
-# cython: language_level=3, linetrace=True
+# cython: language_level=3, linetrace=True, boundscheck=False, wraparound=True
 
 # Copyright (c) 2020, Martin Larralde
 # Copyright (c) 2013-2016, Dirko Coetsee
 
 from libc.stdint cimport int32_t, uint32_t, int64_t
-from numpy cimport ndarray
+from libc.stddef cimport size_t
+from numpy cimport ndarray, float64_t
 from numpy.math cimport INFINITY as inf
 
 import numpy
@@ -19,111 +20,139 @@ cdef extern from "logaddexp.h":
 
 
 cpdef forward_backward(
-    ndarray[double, ndim=3] x_dot_parameters,
-    ndarray[double, ndim=3] state_parameters,
-    ndarray[double, ndim=1] transition_parameters,
+    ndarray[float64_t, ndim=3] x_dot_parameters,
+    ndarray[float64_t, ndim=3] state_parameters,
+    ndarray[float64_t, ndim=1] transition_parameters,
     ndarray[int64_t, ndim=2] transitions,
 ):
-    cdef uint32_t n_time_steps = x_dot_parameters.shape[0]
-    cdef uint32_t n_states = state_parameters.shape[1]
-    cdef uint32_t n_classes = state_parameters.shape[2]
+    cdef uint32_t class_number, s0, s1
+    cdef float64_t edge_potential
+    cdef size_t n_classes, n_states, n_time_steps, n_transitions, t
+    cdef ndarray[float64_t, ndim=3] forward_table, backward_table
+    cdef ndarray[float64_t, ndim=4] forward_transition_table
 
-    cdef uint32_t n_transitions = transitions.shape[0]
+    # Extract dimensions of the input tables
+    n_time_steps = x_dot_parameters.shape[0]
+    n_states = state_parameters.shape[1]
+    n_classes = state_parameters.shape[2]
+    n_transitions = transitions.shape[0]
 
     # Add extra 1 time step for start state
-    cdef ndarray[double, ndim=3] forward_table = numpy.full((n_time_steps + 1, n_states, n_classes), fill_value=-inf, dtype='float64')
-    cdef ndarray[double, ndim=4] forward_transition_table = numpy.full((n_time_steps + 1, n_states, n_states, n_classes), fill_value=-inf, dtype='float64')
+    forward_table = numpy.full(
+        (n_time_steps + 1, n_states, n_classes),
+        fill_value=-inf,
+        dtype='float64'
+    )
+    forward_transition_table = numpy.full(
+        (n_time_steps + 1, n_states, n_states, n_classes),
+        fill_value=-inf,
+        dtype='float64'
+    )
+    backward_table = numpy.full(
+        (n_time_steps + 1, n_states, n_classes),
+        fill_value=-inf,
+        dtype='float64'
+    )
     forward_table[0, 0, :] = 0.0
+    backward_table[n_time_steps, n_states-1, :] = 0.0
 
-    cdef ndarray[double, ndim=3] backward_table = numpy.full((n_time_steps + 1, n_states, n_classes), fill_value=-inf, dtype='float64')
-    backward_table[-1, -1, :] = 0.0
-
-    cdef uint32_t class_number, s0, s1
-    cdef int32_t t
-    cdef double edge_potential
-
-    for t in range(1, n_time_steps + 1):
-        for transition in range(n_transitions):
-            class_number = transitions[transition, 0]
-            s0 = transitions[transition, 1]
-            s1 = transitions[transition, 2]
-            edge_potential = forward_table[t - 1, s0, class_number] + transition_parameters[transition]
-            forward_table[t, s1, class_number] = logaddexp(forward_table[t, s1, class_number],
-                                                                 edge_potential + x_dot_parameters[t - 1, s1, class_number])
-            forward_transition_table[t, s0, s1, class_number] = logaddexp(forward_transition_table[t, s0, s1, class_number],
-                                                                                edge_potential +
-                                                                                x_dot_parameters[t - 1, s1, class_number])
-
-    for t in range(n_time_steps - 1, -1, -1):
-        for transition in range(n_transitions):
-            class_number = transitions[transition, 0]
-            s0 = transitions[transition, 1]
-            s1 = transitions[transition, 2]
-            edge_potential = (backward_table[t + 1, s1, class_number] + x_dot_parameters[t, s1, class_number])
-            backward_table[t, s0, class_number] = logaddexp(backward_table[t, s0, class_number],
-                                                                  edge_potential + transition_parameters[transition])
+    with nogil:
+        for t in range(1, n_time_steps + 1):
+            for transition in range(n_transitions):
+                class_number = transitions[transition, 0]
+                s0 = transitions[transition, 1]
+                s1 = transitions[transition, 2]
+                edge_potential = forward_table[t - 1, s0, class_number] + transition_parameters[transition]
+                forward_table[t, s1, class_number] = logaddexp(
+                    forward_table[t, s1, class_number],
+                    edge_potential + x_dot_parameters[t - 1, s1, class_number]
+                )
+                forward_transition_table[t, s0, s1, class_number] = logaddexp(
+                    forward_transition_table[t, s0, s1, class_number],
+                    edge_potential + x_dot_parameters[t - 1, s1, class_number]
+                )
+        for t in range(n_time_steps - 1, -1, -1):
+            for transition in range(n_transitions):
+                class_number = transitions[transition, 0]
+                s0 = transitions[transition, 1]
+                s1 = transitions[transition, 2]
+                edge_potential = backward_table[t + 1, s1, class_number] + x_dot_parameters[t, s1, class_number]
+                backward_table[t, s0, class_number] = logaddexp(
+                    backward_table[t, s0, class_number],
+                    edge_potential + transition_parameters[transition]
+                )
 
     return forward_table, forward_transition_table, backward_table
 
-def dummy():
-    pass
 
 cpdef log_likelihood(
-    ndarray[double, ndim=2] x,
+    ndarray[float64_t, ndim=2] x,
     int64_t cy,
-    ndarray[double, ndim=3] state_parameters,
-    ndarray[double, ndim=1] transition_parameters,
+    ndarray[float64_t, ndim=3] state_parameters,
+    ndarray[float64_t, ndim=1] transition_parameters,
     ndarray[int64_t, ndim=2] transitions,
 ):
-    cdef uint32_t n_time_steps = x.shape[0]
-    cdef uint32_t n_features = x.shape[1]
-    cdef uint32_t n_states = state_parameters.shape[1]
-    cdef uint32_t n_classes = state_parameters.shape[2]
-    cdef ndarray[double, ndim=3] x_dot_parameters = x.dot(state_parameters.reshape(n_features, -1)).reshape((n_time_steps, n_states, n_classes))
 
-    cdef ndarray[double, ndim=3] forward_table
-    cdef ndarray[double, ndim=4] forward_transition_table
-    cdef ndarray[double, ndim=3] backward_table
+    cdef uint32_t n_time_steps, n_features, n_states, n_classes
+    cdef ndarray[float64_t, ndim=3] backward_table, forward_table, x_dot_parameters
+    cdef ndarray[float64_t, ndim=4] forward_transition_table
 
-    (forward_table,
-     forward_transition_table,
-     backward_table) = forward_backward(x_dot_parameters,
-                                        state_parameters,
-                                        transition_parameters,
-                                        transitions)
-    n_time_steps = forward_table.shape[0] - 1
-    cdef uint32_t n_transitions = transitions.shape[0]
-    cdef ndarray[double, ndim=3] dstate_parameters = numpy.zeros_like(state_parameters, dtype='float64')
-    cdef ndarray[double, ndim=1] dtransition_parameters = numpy.zeros_like(transition_parameters, dtype='float64')
+    n_time_steps = x.shape[0]
+    n_features = x.shape[1]
+    n_states = state_parameters.shape[1]
+    n_classes = state_parameters.shape[2]
+    x_dot_parameters = (
+        x.dot(state_parameters.reshape(n_features, -1))
+          .reshape((n_time_steps, n_states, n_classes))
+    )
+    forward_table, forward_transition_table, backward_table = forward_backward(
+        x_dot_parameters,
+        state_parameters,
+        transition_parameters,
+        transitions
+    )
 
-    cdef ndarray[double, ndim=1] class_Z = numpy.empty((n_classes,))
-    cdef double Z = -inf
+    cdef uint32_t n_transitions
+    cdef ndarray[float64_t, ndim=3] dstate_parameters
+    cdef ndarray[float64_t, ndim=1] dtransition_parameters, class_Z
+
+    n_transitions = transitions.shape[0]
+    dstate_parameters = numpy.zeros_like(state_parameters, dtype='float64')
+    dtransition_parameters = numpy.zeros_like(transition_parameters, dtype='float64')
+
+    class_Z = numpy.empty((n_classes,))
+    cdef float64_t Z = -inf
     cdef uint32_t c
-    for c in range(n_classes):
-        class_Z[c] = forward_table[-1, -1, c]
-        Z = logaddexp(Z, forward_table[-1, -1, c])
+    with nogil:
+        for c in range(n_classes):
+            class_Z[c] = forward_table[-1, -1, c]
+            Z = logaddexp(Z, forward_table[-1, -1, c])
 
-    cdef uint32_t t, state, transition, s0, s1
-    cdef double alphabeta
+
+    cdef uint32_t t, state, transition
+    cdef float64_t alphabeta
     for t in range(1, n_time_steps + 1):
         for state in range(n_states):
             for c in range(n_classes):
                 alphabeta = forward_table[t, state, c] + backward_table[t, state, c]
                 if c == cy:
-                    dstate_parameters[:, state, c] += ((exp(alphabeta - class_Z[c]) -
-                                                        exp(alphabeta - Z)) * x[t - 1, :])
+                    dstate_parameters[:, state, c] += (
+                        (exp(alphabeta - class_Z[c]) - exp(alphabeta - Z)) * x[t - 1, :]
+                    )
                 else:
                     dstate_parameters[:, state, c] -= exp(alphabeta - Z) * x[t - 1, :]
 
-    for t in range(1, n_time_steps + 1):
-        for transition in range(n_transitions):
-            c = transitions[transition, 0]
-            s0 = transitions[transition, 1]
-            s1 = transitions[transition, 2]
-            alphabeta = forward_transition_table[t, s0, s1, c] + backward_table[t, s1, c]
-            if c == cy:
-                dtransition_parameters[transition] += (exp(alphabeta - class_Z[c]) - exp(alphabeta - Z))
-            else:
-                dtransition_parameters[transition] -= exp(alphabeta - Z)
+    cdef uint32_t s0, s1
+    with nogil:
+        for t in range(1, n_time_steps + 1):
+            for transition in range(n_transitions):
+                c = transitions[transition, 0]
+                s0 = transitions[transition, 1]
+                s1 = transitions[transition, 2]
+                alphabeta = forward_transition_table[t, s0, s1, c] + backward_table[t, s1, c]
+                if c == cy:
+                    dtransition_parameters[transition] += (exp(alphabeta - class_Z[c]) - exp(alphabeta - Z))
+                else:
+                    dtransition_parameters[transition] -= exp(alphabeta - Z)
 
     return class_Z[cy] - Z, dstate_parameters, dtransition_parameters
